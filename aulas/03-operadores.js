b7d0d78d546264d86b19fe930bf6d85d2db7f4a7
soaressofia <zatanna434@gmail.com>
2026-03-11 11:14:33 -0300

    Atribuindo uma variável e modificando seu valor posteriormente Operadores inline

---
+++ aulas/03-operadores.js
@@ -0,0 +1,55 @@
+//Trabalhando com operadores aritméticos
+//Definindo variáveis para Trabalhando
+let x = 8;
+let y = 5;
+console.log("x =", x, "\ny =", y);
+console.log("----------------");
+
+//Definindo uma variável para soma
+let soma = x + y; //declarei uma variável "soma" e somei os valores de x e y
+console.log("A soma de x + y é", soma); //mostrei o valor da soma
+
+//DESAFIO 01 - CRIAR OS OUTROS 3 OPERADORES ARITMÉTICOS
+//    (subtração -) (multiplicação *) (divisão /)
+
+//Definindo uma variável para subtração
+let subtracao = x - y; //declarei uma variável "subtração" e somei os valores de x e y
+console.log("A subtração de x - y é", subtracao); //mostrei o valor da soma
+
+//Definindo uma variável para subtração
+let multiplicacao = x * y; //declarei uma variável "multiplicação" e somei os valores de x e y
+console.log("A multiplicação de x * y é", multiplicacao); //mostrei o valor da soma
+
+//Definindo uma variável para subtração
+let divisao = x / y; //declarei uma variável "divisão" e somei os valores de x e y
+console.log("A divisão de x / y é", divisao); //mostrei o valor da soma
+
+//somando direto no console.log
+console.log("Somando números de 1 a 5 =", 1 + 2 + 3 + 4 + 5);
+
+
+//Operações Aritiméticas
+let v1 = 12.5;
+let v2 = 4.5;
+
+console.log("------------------------------------");
+console.log("-> Valores: \n-> v1 =", v1, "\n-> v2 =", v2);
+console.log("------------------------------------");
+
+//calculando com operadores em linha "Inline Code"
+console.log("Soma: \n-> v1 + v2 =", v1 + v2);
+console.log("------------------------------------")
+console.log("Subtração: \n-> v1 - v2 =", v1 - v2);
+console.log("------------------------------------")
+console.log("Multiplicação: \n-> v1 x v2 =", v1 * v2);
+console.log("------------------------------------")
+console.log("Divisão: \n-> v1 / v2 =", v1 / v2);
+console.log("------------------------------------")
+
+//alterando o valor da variavel inicial
+v1 = 10
+v2 = 3
+
+console.log("------------------------------------");
+console.log("-> Valores: \n-> v1 =", v1, "\n-> v2 =", v2);
+console.log("------------------------------------");
