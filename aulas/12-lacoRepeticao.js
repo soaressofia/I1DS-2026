daa734d5f22e549b7404ccb6db1d1c4bf5308d49
soaressofia <zatanna434@gmail.com>
2026-03-24 11:09:32 -0300

    Aprendendo laços de repetição

--- aulas/12-lacoRepeticao.js
+++ aulas/12-lacoRepeticao.js
@@ -2,6 +2,38 @@
 // O professor te colocou de castigo e pediu para você escrever mil vezes a frase
 // "Eu vou prestar atenção às aulas e anotar tudo!"
 
-for (let i = 0; i < 1000; i++) { // i++ == i = i+1
-      
+// ley i = 0 => primeiro utilizo uma variável de controle
+// i < 1000 => condição da Repetição
+// i++ => aumenta a variável de controle para não travar e somar
+for (let i = 0; i < 1000; i++) { 
+    console.log( "Eu vou prestar atenção às aulas e anotar tudo!");
 }
+
+console.log("-------------------------------------------------");
+// Escreva todos os números de 1 a 20
+for (let i = 1; i <= 20; i++) {
+    console.log(i);
+}
+
+// Escreva todos os números pares entre 1 e 20
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+// outra solução de números pares
+for (let i = 0; i <= 20; i += 2) {
+    if (i % 2 == 0) console.log(i);
+    // % => resto da divisão
+    // i % 2 == 0 => se o resto da divisão de i por 2 for 0, ele mostra i
+}
+console.log("------------------------------------------------------------------");
+// Dado um vetor, calcule e exiba o somatório de seus elementos
+let numeros = [ 5, 6, 8, 14, 0, 6, 9, 7, 2 ];
+let soma = 0;
+
+console.log(numeros.length)
+        // numeros.length => mostra a quantidade de itens do array
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
+}
+console.log(soma);
