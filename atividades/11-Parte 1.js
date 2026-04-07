5e789390f0fb545d8b8129af177588024d6652e4
soaressofia <zatanna434@gmail.com>
2026-04-07 10:56:04 -0300

    Parte 1 da lista de exercicios

---
+++ atividades/11-Parte 1.js
@@ -0,0 +1,153 @@
+//Criando variáveis com "var" (var nome_variavel = <valor>;)
+var nome = "Sofia";
+
+//Criando variável de idade e atribuindo uma idade para ela
+var idade = 16;
+
+//Criando a variável cidade
+var cidade = "Jaú";
+
+//Demonstrando no console várias informações e pulo de linha com \n
+console.log("Meu nome é", nome, "e eu tenho", idade, "anos", "e moro em", cidade);
+
+console.log("-----------------------------------")
+
+let x = 22;
+let y = 5;
+
+console.log("X = ", x, "| Y = ", y);
+
+resultado = x + y ;
+console.log("A soma é",resultado);
+
+resultado = (x - y) ;
+console.log("A subtração é",resultado);
+
+resultado = (x * y) ;
+console.log("A multiplicação é",resultado);
+
+resultado = (x / y );
+console.log("A divisão é", resultado);
+
+console.log("----------------------------------------------------")
+
+
+
+// 2.Criando variáveis numéricas
+console.log("X = ", x, "| Y = ", y);
+
+resultado = x + y;
+console.log("A soma é", resultado);
+
+resultado = x - y;
+console.log("A subtração é", resultado);
+
+resultado = x * y;
+console.log("A multiplicação é", resultado);
+
+resultado = x / y;
+console.log("A divisão é", resultado);
+console.log("---------------------------------------------------");
+
+
+
+// 3.Calcule a área de um retângulo
+let base = 10;
+let hRetangulo = 5;
+
+let area = (base * hRetangulo);
+
+console.log("A base do retângulo é:", base);
+console.log("A altura do retângulo é:", hRetangulo);
+console.log("Então a área do retângulo é:", area);
+console.log("---------------------------------------------------");
+
+
+
+// 4.Calcule o volume do paralelepípedo
+let comprimento = 10;
+let largura = 5;
+let hParalelepipedo = 3;
+
+let volumeP = comprimento * largura * hParalelepipedo;
+
+console.log("O comprimento é:", comprimento);
+console.log("A largura é", largura);
+console.log("A altura é:", hParalelepipedo);
+console.log("O Volume do paralelepípedo é:", volumeP);
+console.log("---------------------------------------------------");
+
+
+
+// 5.Calcule 10% do desconto
+let valor = 200;
+let multiplicacao = 10;
+let divisao = 100;
+
+let desconto = (valor * multiplicacao) / divisao;
+
+console.log("O valor do produto é R$:", valor, "reais");
+console.log(
+  "Com o desconto de 10% o produto saira por R$:",
+  valor - desconto,
+  "reais",
+);
+console.log("---------------------------------------------------");
+
+
+
+// 7.Calcule o IMC
+let peso = 70;
+let altura = 1.7;
+
+let IMC = peso / (altura * altura);
+
+console.log("O seu peso é:", peso);
+console.log("A sua altura é:", altura);
+console.log("Então o seu IMC é:", IMC);
+console.log("-----------------------------------------------");
+
+
+
+// 8.Calculando a média
+let n1 = 8;
+let n2 = 10;
+let n3 = 6;
+
+let media = (n1 + n2 + n3) / 4;
+
+console.log("Nota da primeira prova:", n1);
+console.log("Nota da segunda prova:", n2);
+console.log("Nota da terceira prova:", n3);
+console.log("A sua média é:", media);
+console.log("---------------------------------------------------");
+
+
+
+// 9.Calcule o valor de delta de uma equção de 2° grau
+let a = 1;
+let b = -5;
+let c = 6;
+
+let delta = b**2 - 4*a*c;
+
+console.log("-----------------------------");
+console.log("-> Valor de a:", a);
+console.log("-> Valor de b:", b);
+console.log("-> Valor de c:", c)
+console.log("-> O valor de delta é:", delta)
+console.log("-----------------------------");
+
+
+
+// 10.Dado o valor em hora converta pra minutos e segundos 
+let horas = 2
+let minutos = (horas * 60)
+let segundos = (horas * 3600)
+
+ console.log("Horas:", horas,);
+ console.log("Minutos:", minutos);
+console.log("Segundos:", segundos);
+console.log("---------------------------------------------------");
+
+
